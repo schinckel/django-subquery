9e05092abd7989bccdda708fd03ad4554d0dc988
Matthew Schinckel <matt@schinckel.net>
2017-03-07 12:45:01 +1030

    Bump version to update GitHub link.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-subquery',
-    version='1.0.1',
+    version='1.0.2',
     description='SubQuery support in Django < 1.11',
     url='https://github.com/schinckel/django-subquery/',
     author='Matthew Schinckel',
